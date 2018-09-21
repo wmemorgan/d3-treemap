9b379a6c91dc79d6537eee7e3645833690007d2c
Wilfred Morgan <wil.me.morgan@gmail.com>
2018-09-21 17:03:14 +0800

    Apply chart template

---
+++ script.js
@@ -0,0 +1,40 @@
+// Data Visualization Script
+
+// Set the margin and padding of the SVG
+const margin = { top: 50, right: 20, bottom: 50, left: 100 }
+const padding = 60
+
+// Set the width and height using the current width and height of the div
+const width = 960
+const height = 600
+
+// Create svg and append to chart div
+const svg = d3.select('#chart')
+  .append('svg')
+  .attr('class', 'graph')
+  .attr('width', width + margin.left + margin.right)
+  .attr('height', height + margin.top + margin.bottom)
+
+// Title
+svg.append('text')
+  .text('My Treemap (Working Title)')
+  .attr('id', 'title')
+  .attr(`transform`, `translate(${width / 2}, ${padding / 2})`)
+
+// Description
+svg.append('text')
+  .text(`Top XX Most XX Grouped by XX`)
+  .attr('id', 'description')
+  .attr(`transform`, `translate(${width / 2}, ${padding / 1})`)
+
+// Tooltip  
+const tooltip = d3.select('#chart').append('div')
+  .attr('id', 'tooltip')
+  .style('opacity', 0)
+
+
+// Get Data
+const chart = async () => {
+
+}
+chart()
